1680b6c33a625d8bc30090624e63e3231648f2af
Christian Minatti <christian@minatti.tirol>
2024-03-05 16:55:32 +0100

    todo-api added

---
+++ libs/server/feature-todo/src/lib/server-feature-todo.controller.ts
@@ -0,0 +1,25 @@
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { ServerFeatureTodoService } from './server-feature-todo.service';
+import { ITodo } from '@fst/shared/domain';
+import { CreateTodoDto } from './dto/todo.dto';
+
+@Controller('server-feature-todo')
+export class ServerFeatureTodoController {
+  constructor(private serverFeatureTodoService: ServerFeatureTodoService) {
+  }
+
+  @Get('')
+  getAll(): ITodo[] {
+    return this.serverFeatureTodoService.getAll();
+  }
+
+  @Get(':id')
+  getOne(@Param('id') id: string): ITodo {
+    return this.serverFeatureTodoService.getOne(id);
+  }
+
+  @Post('')
+  create(@Body() data: CreateTodoDto): ITodo {
+    return this.serverFeatureTodoService.create(data);
+  }
+}
